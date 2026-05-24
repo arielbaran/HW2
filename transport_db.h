0731519c90782d677bb820522d82d474de097140
Ilan Chelly <ilanchelly@gmail.com>
2026-05-24 13:18:29 +0300

    Tidy transport header spacing

--- transport_db.h
+++ transport_db.h
@@ -10,10 +10,8 @@ void TransportDestroy(TransportDB *tdb);
 
 TransportResult TransportAddLine(TransportDB *tdb, const char *type, int line_id, float price);
 TransportResult TransportRemoveLine(TransportDB *tdb, int line_id);
-
 TransportResult TransportAddStation(TransportDB *tdb, int line_id, const char *station);
 TransportResult TransportRemoveStation(TransportDB *tdb, int line_id, unsigned int index);
-
 TransportResult TransportReportLines(TransportDB *tdb, const char *type);
 TransportResult TransportReportStations(TransportDB *tdb, int line_id);
 TransportResult TransportReportDirections(TransportDB *tdb, const char *from, const char *to);
